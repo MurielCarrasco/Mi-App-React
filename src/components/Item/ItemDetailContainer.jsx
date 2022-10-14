bedf4aabf91ca369af3c6d35a0683ebb3e50bd88
murielcarrasco <muriel.c@hotmail.com>
2022-10-14 03:59:52 -0300

    arreglando itemDetail

--- src/components/Item/ItemDetailContainer.jsx
+++ src/components/Item/ItemDetailContainer.jsx
@@ -6,17 +6,30 @@ import ItemDetail from "./ItemDetail";
 import { useParams } from "react-router-dom";
 import { getFirestore, doc, getDoc } from "firebase/firestore";
 
-const ItemDetailContainer = () => {
+export const ItemDetailContainer = () => {
   const [producto, setProducto] = useState({});
   const { detalleId } = useParams();
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     const querybd = getFirestore();
-    const queryDoc = doc(querybd, "productos", detalleId);
-    getDoc(queryDoc).then((res) => setProducto({ id: res.id, ...res.data() }));
+    const queryDoc = doc(querybd, "productos", `${detalleId}`);
+    getDoc(queryDoc).then((res) => {
+      console.log(res)
+      if(res.exists()){
+        setProducto({ id: res.id, ...res.data() });
+        setLoading(false)
+      }
+    });
   }, [detalleId]);
 
-  return <ItemDetail item={producto} />;
+  return(
+    <div>
+      { loading ? "loading"
+      :
+      <ItemDetail item={producto} />};
+    </div>
+  ) 
 };
 
 export default ItemDetailContainer;
